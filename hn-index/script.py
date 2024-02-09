1f0a09b7b94c08065f4e381c3a46419aabba4c0e
Alex Molas <alexmolasmartin@gmail.com>
2024-02-09 23:25:55 +0100

    hn index post

--- hn-index/script.py
+++ hn-index/script.py
@@ -1,9 +1,9 @@
 import argparse
-from collections import defaultdict
 import aiohttp
 import asyncio
 from tqdm import tqdm
-from pprint import pprint
+import numpy as np
+
 
 async def fetch_user_data(session, url):
     async with session.get(url) as response:
@@ -16,10 +16,10 @@ async def fetch_item_data(session, url, scores, progress_bar):
         if (
             (not item.get("deleted"))
             and (not item.get("dead"))
-            and (t := item.get("type"))
+            and (item.get("type") == "story")
             and (s := item.get("score"))
         ):
-            scores[t].append(int(s))
+            scores.append(int(s))
         progress_bar.update(1)
 
 
@@ -27,8 +27,8 @@ async def get_user_scores(username):
     user_url = (
         f"https://hacker-news.firebaseio.com/v0/user/{username}.json?print=pretty"
     )
-
-    async with aiohttp.ClientSession() as session:
+    connector = aiohttp.TCPConnector(limit=1000)
+    async with aiohttp.ClientSession(connector=connector) as session:
         user = await fetch_user_data(session, user_url)
 
         item_urls = [
@@ -36,12 +36,12 @@ async def get_user_scores(username):
             for sub in user["submitted"]
         ]
 
-        scores = defaultdict(list)
+        scores = []
         with tqdm(total=len(item_urls), desc=f"Fetching {username}'s scores") as pbar:
             tasks = [fetch_item_data(session, url, scores, pbar) for url in item_urls]
             await asyncio.gather(*tasks)
 
-    return scores
+    return user["karma"], scores 
 
 
 def h_index(scores):
@@ -50,13 +50,22 @@ def h_index(scores):
 
 def parse_args():
     parser = argparse.ArgumentParser()
-    parser.add_argument("--user")
+    parser.add_argument("--user", default=None)
+    parser.add_argument("--users-path", default=None)
     return parser.parse_args()
 
 
 if __name__ == "__main__":
     args = parse_args()
-    username = args.user
-    scores_result = asyncio.run(get_user_scores(username))
-    for t, s in scores_result.items():
-        print(t, h_index(s))
+    if username := args.user:
+        karma, scores_result = asyncio.run(get_user_scores(username))
+        print(username, karma, h_index(scores_result))
+    if users_path := args.users_path:
+        output_path = f"{users_path.split('.')[0]}-output.csv"
+
+        with open(users_path) as f:
+            for username in f.readlines():
+                username = username.strip("\n")
+                karma, scores_result = asyncio.run(get_user_scores(username))
+                with open(output_path, "a+") as f:
+                    f.write(f"{username},{len(scores_result)},{h_index(scores_result)},{karma}\n")
